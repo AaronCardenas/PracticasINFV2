99426a98aa2e1e93e2590fe788e4a992f58bc088
Aaron Cardenas <aaron.cardenas@alumnos.uv.cl>
2024-01-18 03:53:07 -0300

    Endpoint Unirdatos retorna PDF

---
+++ backend/app/controllers/data.controller.js
@@ -0,0 +1,27 @@
+const db = require("../models");
+const jwt = require('jsonwebtoken');
+const key = require('../config/const.js').JWT_SECRET;
+const { conversion } = require('./conversion.controller.js');
+
+
+exports.unirDatos = async (req,res) => {
+    const { rutUsuario, rutEmpresa } = req.body;
+    const usuario = await db.usuario.findOne({where:{rut:rutUsuario}});
+    const empresa = await db.empresa.findOne({where:{rutEmpresa:rutEmpresa}});
+
+    const datosFormatoJSON = {
+          count: 3,
+          razonSocial: empresa.razonSocial,
+          direccion: empresa.direccion,
+          region : empresa.region,
+          rut: usuario.rut,
+          semestre: 2,
+          horas: 320,
+          numeroPractica: "segunda",
+          nombre1: usuario.nombre1,
+          nombre2: usuario.nombre2,
+          apellido1: usuario.apellido1,
+          apellido2: usuario.apellido2,
+      };
+      await conversion({ body: datosFormatoJSON }, res);
+};
--- backend/app/routes/utils.router.js
+++ backend/app/routes/utils.router.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // Importacion de controladores (endpoints simples)
 const { conversion } = require('../controllers/conversion.controller.js');
-
+const { unirDatos } = require('../controllers/data.controller.js');
 //Ruta de prueba
 router.get("/",(req,res)=>{
     res.json({message:"Ruta de /utils/ funcionando"});
@@ -11,4 +11,5 @@ router.get("/",(req,res)=>{
 // Ruta de conversion                               // DATOS JSON:
 router.post("/conversion",conversion);              // {count, razonSocial, direccion, region, rut, semestre, horas, numeroPractica, nombre1, nombre2, apellido1, apellido2}
 
+router.post("/unirDatos",unirDatos);              // {rutUsuario, rutEmpresa}
 module.exports = router;
