15abb00fd3fc9c14d74f293c0ebb02e924856b9d
SwTzu <morbemon2012@gmail.com>
2024-03-18 10:49:09 -0300

    consulta atumatica de fechas

--- backend/app/controllers/solicitud.controller.js
+++ backend/app/controllers/solicitud.controller.js
@@ -1,28 +1,27 @@
-const db = require("../models");
+const db = require('../models');
 const key = require('../config/const.js').JWT_SECRET;
 const jwt = require('jsonwebtoken');
 const Op = db.Sequelize.Op;
 
 const supXest = async (req, res) => {
   try {
+    const { token } = req.body;
+    const usuario = await jwt.verify(token, key);
 
-      const { token } = req.body;
-      const usuario = await jwt.verify(token, key);
+    const supervisor = await db.supervisor.findOne({
+      where: { correoSupervisor: usuario.correoSupervisor },
+    });
 
-      const supervisor = await db.supervisor.findOne(
-          {where: { correoSupervisor : usuario.correoSupervisor }}
-      );
-      
-      const solicitudes = await db.solicitud.findAll(
-          {where: { correoSupervisor: supervisor.correoSupervisor }}
-      );
+    const solicitudes = await db.solicitud.findAll({
+      where: { correoSupervisor: supervisor.correoSupervisor },
+    });
 
-      return res.status(200).json({
-          message: "Solicitudes encontradas exitosamente",
-          solicitudes
-      });
+    return res.status(200).json({
+      message: 'Solicitudes encontradas exitosamente',
+      solicitudes,
+    });
 
-      /*
+    /*
         const empresa = await db.empresa.findOne(
           {where: { rutEmpresa : supervisor.rutEmpresa }}
         );
@@ -35,23 +34,20 @@ const supXest = async (req, res) => {
       });
       */
   } catch (err) {
-      return res.status(500).json({
-          err
-      });
+    return res.status(500).json({
+      err,
+    });
   }
 };
 const crearSolicitud = async (req, res) => {
-
   const { token, datos } = req.body;
   const { rut } = jwt.verify(token, key);
-  console.log("datos de practica",datos);
+  console.log('datos de practica', datos);
 
-  const numeroPractica = datos.numeroPractica; 
-  
+  const numeroPractica = datos.numeroPractica;
+
   const solicitudCalificada = await db.solicitud.findOne({
-
     where: { rut, fase: 5, numeroPractica }, // En entero, la fase calificada es 5
-
   });
   const solicitudAceptada = await db.solicitud.findOne({
     where: { rut, fase: 3, numeroPractica }, // En entero, la fase aceptada es 3
@@ -62,15 +58,15 @@ const crearSolicitud = async (req, res) => {
 
   if (solicitudCalificada) {
     return res.status(409).json({
-      message: "Ya se ha realizado esta práctica profesional",
+      message: 'Ya se ha realizado esta práctica profesional',
     });
   } else if (solicitudAceptada) {
     return res.status(409).json({
-      message: "Ya se ha aceptado esta práctica profesional",
+      message: 'Ya se ha aceptado esta práctica profesional',
     });
   } else if (solicitudPracticaAnteriorNoTerminada) {
     return res.status(409).json({
-      message: "Aun no se termina la práctica profesional anterior",
+      message: 'Aun no se termina la práctica profesional anterior',
     });
   }
 
@@ -84,51 +80,49 @@ const crearSolicitud = async (req, res) => {
     });
 
     return res.status(201).json({
-      message: "Solicitud creada exitosamente",
+      message: 'Solicitud creada exitosamente',
       solicitud,
     });
-  }catch (error) {
+  } catch (error) {
     console.error('Error al crear solicitud:', error);
     return res.status(500).json({
-      message: "Error interno del servidor al crear la solicitud",
+      message: 'Error interno del servidor al crear la solicitud',
     });
   }
 };
 
 const eliminarSolicitud = async (req, res) => {
-
   const { token, idSolicitud } = req.body;
 
-  try{
-
+  try {
     const { rut } = jwt.verify(token, key);
 
-    const solicitud = await db.solicitud.findOne({ where: { idSolicitud: idSolicitud } });
+    const solicitud = await db.solicitud.findOne({
+      where: { idSolicitud: idSolicitud },
+    });
 
     if (!solicitud) {
       return res.status(404).json({
-        message: "Solicitud no encontrada",
+        message: 'Solicitud no encontrada',
       });
     }
 
-    if(solicitud.rut !== rut){
+    if (solicitud.rut !== rut) {
       return res.status(409).json({
-        message: "No tienes permisos para eliminar esta solicitud",
+        message: 'No tienes permisos para eliminar esta solicitud',
       });
     }
 
     await solicitud.destroy();
     return res.status(200).json({
-      message: "Solicitud eliminada exitosamente",
+      message: 'Solicitud eliminada exitosamente',
     });
-
-  }
-  catch(err){
+  } catch (err) {
     return res.status(500).json({
-      message: "Error interno del servidor",
+      message: 'Error interno del servidor',
       err,
     });
-  };
+  }
 };
 
 const faseSolicitud = async (req, res) => {
@@ -137,22 +131,25 @@ const faseSolicitud = async (req, res) => {
   try {
     const { fase, rechazo } = req.body;
 
-    const solicitud = await db.solicitud.findOne({ where: { idSolicitud: id } });
+    const solicitud = await db.solicitud.findOne({
+      where: { idSolicitud: id },
+    });
 
     if (!solicitud) {
       return res.status(404).json({
-        message: "Solicitud no encontrada",
+        message: 'Solicitud no encontrada',
       });
     }
 
     if (solicitud.fase === 3) {
-
-      if (solicitud.supervisorCheck === false || solicitud.alumnoCheck === false) {
+      if (
+        solicitud.supervisorCheck === false ||
+        solicitud.alumnoCheck === false
+      ) {
         return res.status(409).json({
-          message: "Solicitud no se encuentra lista para avanzar de fase",
+          message: 'Solicitud no se encuentra lista para avanzar de fase',
         });
       }
-
     }
 
     solicitud.fase = fase;
@@ -163,11 +160,11 @@ const faseSolicitud = async (req, res) => {
     await solicitud.save();
 
     return res.status(200).json({
-      message: "Solicitud cambiada de fase exitosamente",
+      message: 'Solicitud cambiada de fase exitosamente',
     });
   } catch (err) {
     return res.status(500).json({
-      message: "Error interno del servidor",
+      message: 'Error interno del servidor',
       err,
     });
   }
@@ -177,55 +174,56 @@ const verSolicitudesUsuario = async (req, res) => {
     const { token } = req.body;
     const usuario = await jwt.verify(token, key);
     console.log(usuario);
-    const solicitudes = await db.solicitud.findAll({ where: { rut: usuario.rut } });
+    const solicitudes = await db.solicitud.findAll({
+      where: { rut: usuario.rut },
+    });
     return res.status(200).json({
-      message: "Solicitudes listadas exitosamente",
-      solicitudes
+      message: 'Solicitudes listadas exitosamente',
+      solicitudes,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Error al listar las solicitudes',
+      err,
     });
   }
-  catch (err) {
-    return res.status(500).json({
-      message: "Error al listar las solicitudes",
-      err
-    })
-  }
-
 };
 // Vista usuario
 const verSolicitudesAceptadasU = async (req, res) => {
   try {
     const { rut } = req.body;
-    const solicitudes = await db.solicitud.findAll({ where: { rut: rut, fase: "Aceptada" } });
+    const solicitudes = await db.solicitud.findAll({
+      where: { rut: rut, fase: 'Aceptada' },
+    });
     const solicitudList = solicitudes.map((solicitud) => {
       return {
         idSolicitud: solicitud.idSolicitud,
         numeroPractica: solicitud.numeroPractica,
-        fase: solicitud.fase
+        fase: solicitud.fase,
       };
     });
     return res.status(200).json({
-      message: "Solicitudes listadas exitosamente",
-      solicitudList
+      message: 'Solicitudes listadas exitosamente',
+      solicitudList,
     });
-  }
-  catch (err) {
+  } catch (err) {
     return res.status(500).json({
-      message: "Error al listar las solicitudes",
-      err
-    })
+      message: 'Error al listar las solicitudes',
+      err,
+    });
   }
 };
 // Vista coordinador
 const allSolicitudesCoo = async (req, res) => {
   try {
-    const solicitudes = await db.solicitud.findAll({ where: { fase: "3" } });
+    const solicitudes = await db.solicitud.findAll({ where: { fase: '3' } });
     return res.status(200).json({
-      message: "Solicitudes listadas exitosamente",
+      message: 'Solicitudes listadas exitosamente',
       solicitudes,
     });
   } catch (err) {
     return res.status(500).json({
-      message: "Error al listar las solicitudes",
+      message: 'Error al listar las solicitudes',
       err,
     });
   }
@@ -233,14 +231,14 @@ const allSolicitudesCoo = async (req, res) => {
 // Vista Jefe de Carrera
 const allSolicitudesJefe = async (req, res) => {
   try {
-    const solicitudes = await db.solicitud.findAll({ where: { fase: "2" } });
+    const solicitudes = await db.solicitud.findAll({ where: { fase: '2' } });
     return res.status(200).json({
-      message: "Solicitudes listadas exitosamente",
+      message: 'Solicitudes listadas exitosamente',
       solicitudes,
     });
   } catch (err) {
     return res.status(500).json({
-      message: "Error al listar las solicitudes",
+      message: 'Error al listar las solicitudes',
       err,
     });
   }
@@ -248,20 +246,19 @@ const allSolicitudesJefe = async (req, res) => {
 // Vista Secretaria // No implementada
 const allSolicitudesSec = async (req, res) => {
   try {
-    const solicitudes = await db.solicitud.findAll({ where: { fase: "1" } });
+    const solicitudes = await db.solicitud.findAll({ where: { fase: '1' } });
     return res.status(200).json({
-      message: "Solicitudes listadas exitosamente",
+      message: 'Solicitudes listadas exitosamente',
       solicitudes,
     });
   } catch (err) {
     return res.status(500).json({
-      message: "Error al listar las solicitudes",
+      message: 'Error al listar las solicitudes',
       err,
     });
   }
 };
 const readySupervisor = async (req, res) => {
-
   const { idSolicitud } = req.body;
 
   try {
@@ -269,13 +266,13 @@ const readySupervisor = async (req, res) => {
 
     if (!solicitud) {
       return res.status(404).json({
-        message: "Solicitud no encontrada",
+        message: 'Solicitud no encontrada',
       });
     }
 
     if (solicitud.supervisorCheck == true) {
       return res.status(409).json({
-        message: "Solicitud ya fue marcada como lista por el supervisor",
+        message: 'Solicitud ya fue marcada como lista por el supervisor',
       });
     }
 
@@ -284,34 +281,29 @@ const readySupervisor = async (req, res) => {
       await solicitud.save();
 
       return res.status(200).json({
-        message: "Solicitud actualizada exitosamente. Supervisor listo.",
+        message: 'Solicitud actualizada exitosamente. Supervisor listo.',
       });
-    }
-    else {
+    } else {
       return res.status(409).json({
-        message: "Solicitud no se encuentra en la fase correcta",
+        message: 'Solicitud no se encuentra en la fase correcta',
       });
     }
-
-  }
-  catch (err) {
+  } catch (err) {
     return res.status(500).json({
-      message: "Error interno del servidor",
+      message: 'Error interno del servidor',
       err,
     });
   }
 };
 const readyAlumno = async (req, res) => {
-
   const { idSolicitud } = req.body;
 
   try {
-
     const solicitud = await db.solicitud.findOne({ where: { idSolicitud } });
 
     if (!solicitud) {
       return res.status(404).json({
-        message: "Solicitud no encontrada",
+        message: 'Solicitud no encontrada',
       });
     }
 
@@ -320,82 +312,99 @@ const readyAlumno = async (req, res) => {
       await solicitud.save();
 
       return res.status(200).json({
-        message: "Solicitud actualizada exitosamente. Alumno listo.",
+        message: 'Solicitud actualizada exitosamente. Alumno listo.',
       });
-    }
-    else {
+    } else {
       return res.status(409).json({
-        message: "Solicitud no se encuentra en la fase correcta",
+        message: 'Solicitud no se encuentra en la fase correcta',
       });
     }
-
-  }
-  catch (err) {
+  } catch (err) {
     return res.status(500).json({
-      message: "Error interno del servidor",
+      message: 'Error interno del servidor',
       err,
     });
   }
-
 };
 // aplicar solo usarios con el token de admin pueden usar esta funcion.
 const actualizarFase = async (req, res) => {
-
   const { idSolicitud, nroFase, motivoRechazo } = req.body;
 
   try {
-
     const solicitud = await db.solicitud.findOne({ where: { idSolicitud } });
 
     if (!solicitud) {
       return res.status(404).json({
-        message: "Solicitud no encontrada",
+        message: 'Solicitud no encontrada',
       });
     }
 
-    if (motivoRechazo){
-      solicitud.descripcionRechazo = motivoRechazo
+    if (motivoRechazo) {
+      solicitud.descripcionRechazo = motivoRechazo;
     }
 
     solicitud.fase = nroFase;
     await solicitud.save();
 
     return res.status(200).json({
-      message: "Se realizo correctamente el cambio de fase.",
+      message: 'Se realizo correctamente el cambio de fase.',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      message: 'Error interno del servidor',
+      err,
     });
   }
-
-  catch (err) {
-  return res.status(500).json({
-    message: "Error interno del servidor",
-    err,
-  });
-}
 };
 const agregarSup = async (req, res) => {
-
-  const { token, idSolicitud, correoSupervisor} = req.body;
+  const { token, idSolicitud, correoSupervisor } = req.body;
   const { usuario } = jwt.verify(token, key);
 
-  const solicitud = await db.solicitud.findOne({ where: { idSolicitud: idSolicitud } });
+  const solicitud = await db.solicitud.findOne({
+    where: { idSolicitud: idSolicitud },
+  });
 
   if (!solicitud) {
     return res.status(404).json({
-      message: "Solicitud no encontrada",
+      message: 'Solicitud no encontrada',
     });
-  };
-  
+  }
+
   solicitud.correoSupervisor = correoSupervisor;
 
   await solicitud.save();
 
   return res.status(200).json({
-    message: "Supervisor agregado correctamente",
-    solicitud
+    message: 'Supervisor agregado correctamente',
+    solicitud,
   });
-
 };
+
+const fechaauto = async (req, res) => {
+  const solicitudes = await db.solicitud.findAll({ where: { fase: [6, 7] } });
+  fechahoy = new Date();
+  fechahoy2 = new Date(fechahoy.getTime()+(1000 * 60 * 60 * 24 * 20));
+  solicitudes.forEach(async (element) => {
+    const carta = await db.carta.findOne({
+      where: { idSolicitud: element.idSolicitud },
+    });
+    if(element.fase == 7 && fechahoy2 >= carta.fechaTermino){
+      memoria= await db.memoria.findOne({where: {idSolicitud: element.idSolicitud}});
+      if (memoria == null || memoria.documento == null) {
+        element.fase = 0;
+        element.save();
+      };
+    }else if (fechahoy >= carta.fechaTermino) {
+      element.fase = 7;
+      element.save();
+    } else {
+      console.log('no', element.idSolicitud);
+    }
+  });
+};
+
 module.exports = {
+  fechaauto,
   supXest,
   crearSolicitud,
   eliminarSolicitud,
@@ -407,5 +416,5 @@ module.exports = {
   allSolicitudesSec,
   readyAlumno,
   readySupervisor,
-  actualizarFase
+  actualizarFase,
 };
--- backend/app/routes/solicitud.router.js
+++ backend/app/routes/solicitud.router.js
@@ -1,41 +1,56 @@
 const router = require('express').Router();
+const axios = require('axios');
 
-
-const { 
-    faseSolicitud,
-    verSolicitudesUsuario,
-    verSolicitudesAceptadasU,
-    crearSolicitud,
-    allSolicitudesCoo,
-    allSolicitudesJefe,
-    allSolicitudesSec,
-    readyAlumno,
-    readySupervisor,
-    actualizarFase,
-    supXest,
-    eliminarSolicitud
+const {
+  faseSolicitud,
+  verSolicitudesUsuario,
+  verSolicitudesAceptadasU,
+  crearSolicitud,
+  allSolicitudesCoo,
+  allSolicitudesJefe,
+  allSolicitudesSec,
+  readyAlumno,
+  readySupervisor,
+  actualizarFase,
+  supXest,
+  fechaauto,
+  eliminarSolicitud,
 } = require('../controllers/solicitud.controller.js');
 
-
 //Ruta de prueba
-router.get("/",(req,res)=>{
-    res.json({message:"Ruta de /solicitud/ funcionando"});
+router.get('/', (req, res) => {
+  res.json({ message: 'Ruta de /solicitud/ funcionando' });
 });
 
-
 // Ruta de validacion de usuario                                // DATOS JSON:
-router.post("/crear",crearSolicitud);                           // { rut, rutempresa, extension, numeroPractica ,}
-router.put("/:id",faseSolicitud);                               // {fase, descripcionRechazo (puede ser nulo)}
-router.post("/listaSolicitudes",verSolicitudesUsuario);         // {rut}
-router.post("/SolicitudesAceptadas",verSolicitudesAceptadasU);  // {rut}
-router.get("/allSolicitudesCoo",allSolicitudesCoo);             
-router.get("/allSolicitudesJefe",allSolicitudesJefe);           
-router.get("/allSolicitudesSec",allSolicitudesSec);
-router.post("/readyAlumno", readyAlumno);                        // {idSolicitud}
-router.post("/readySupervisor", readySupervisor); 
-router.put("/actualizar/:id", actualizarFase);               // {idSolicitud, nroFase}
-router.post("/supXest", supXest);                               // {rutSupervisor, rutEstudiante}
-router.post("/eliminar", eliminarSolicitud);                     // {idSolicitud}
-
+router.post('/crear', crearSolicitud); // { rut, rutempresa, extension, numeroPractica ,}
+router.put('/:id', faseSolicitud); // {fase, descripcionRechazo (puede ser nulo)}
+router.post('/listaSolicitudes', verSolicitudesUsuario); // {rut}
+router.post('/SolicitudesAceptadas', verSolicitudesAceptadasU); // {rut}
+router.get('/allSolicitudesCoo', allSolicitudesCoo);
+router.get('/allSolicitudesJefe', allSolicitudesJefe);
+router.get('/allSolicitudesSec', allSolicitudesSec);
+router.post('/readyAlumno', readyAlumno); // {idSolicitud}
+router.post('/readySupervisor', readySupervisor);
+router.put('/actualizar/:id', actualizarFase); // {idSolicitud, nroFase}
+router.post('/supXest', supXest); // {rutSupervisor, rutEstudiante}
+router.post('/eliminar', eliminarSolicitud); // {idSolicitud}
+router.get('/fechaauto', fechaauto);
 
 module.exports = router;
+// const app = express();
+const port = 3000;
+async function hacerSolicitud() {
+  try {
+    const respuesta = await axios.get('http://localhost:3001/solicitud/fechaauto');
+    // console.log('Respuesta:', respuesta.data);
+  } catch (error) {
+    console.error('Error al hacer la solicitud:', error.message);
+  }
+}
+const intervalo = 3600*12*1000;
+function solicitarAutomaticamente() {
+  hacerSolicitud();
+  setTimeout(solicitarAutomaticamente, intervalo);
+}
+solicitarAutomaticamente();
