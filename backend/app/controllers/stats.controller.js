1e063fb707e511b03d16a5ab4221bf232a1a9cb5
SwTzu <morbemon2012@gmail.com>
2024-03-26 11:13:56 -0300

    ahora si lo encuentra el nombre usando el metodo include

--- backend/app/controllers/stats.controller.js
+++ backend/app/controllers/stats.controller.js
@@ -1,35 +1,38 @@
-const db = require("../models");
+const db = require('../models');
 const Op = db.Sequelize.Op;
 
-
 const empresasAprobadas = async (req, res) => {
-    try {
-        const solicitudes = await db.solicitud.findAll({
-            where: {
-                calificacion: {
-                    [db.Sequelize.Op.gte]: 4
-                }
-            }
-        });
-        
-        const empresasRepetidas = {};
-        solicitudes.forEach(async solicitud => {
-            console.log(solicitud.rutEmpresa);
-            const empresaId = await db.empresa.findOne({where: {rutEmpresa: solicitud.rutEmpresa}});
-            console.log(empresaId.razonSocial);
-            if (empresasRepetidas[empresaId]) {
-                empresasRepetidas[empresaId]++;
-            } else {
-                empresasRepetidas[empresaId] = 1;
-            }
-        });
-        res.status(200).json(empresasRepetidas);
-        
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+  try {
+    const solicitudes = await db.solicitud.findAll({
+      include: [
+        {
+          model: db.empresa,
+          as: 'empresa',
+        },
+      ],
+      where: {
+        calificacion: {
+          [db.Sequelize.Op.gte]: 4,
+        },
+      },
+    });
+
+    const empresasRepetidas = {};
+    solicitudes.forEach(async (solicitud) => {
+      console.log(solicitud.empresa.razonSocial);
+      if (empresasRepetidas[solicitud.empresa.razonSocial]) {
+        empresasRepetidas[solicitud.empresa.razonSocial]++;
+      } else {
+        empresasRepetidas[solicitud.empresa.razonSocial] = 1;
+      }
+    });
+
+    res.status(200).json(empresasRepetidas);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 module.exports = {
-    empresasAprobadas
+  empresasAprobadas,
 };
